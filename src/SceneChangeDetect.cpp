7833efbce1ed248af1c3fd35cbd3024c0ea8724f
Internax <matatyrek@gmail.com>
2025-07-14 12:09:16 +0200

    name conflict resolve

---
+++ src/SceneChangeDetect.cpp
@@ -0,0 +1,8 @@
+//
+//  SceneChangeDetect.cpp
+//  Film_type_classifier
+//
+//  Created by Marek Tatýrek on 14.07.2025.
+//
+
+#include "SceneChangeDetect.hpp"
---
+++ src/SceneChangeDetect.hpp
@@ -0,0 +1,13 @@
+//
+//  SceneChangeDetect.hpp
+//  Film_type_classifier
+//
+//  Created by Marek Tatýrek on 14.07.2025.
+//
+
+#ifndef SceneChangeDetect_hpp
+#define SceneChangeDetect_hpp
+
+#include <stdio.h>
+
+#endif /* SceneChangeDetect_hpp */

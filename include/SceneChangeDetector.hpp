02984c5978f9a598a36dbecd1c61dbc914c317ec
Internax <matatyrek@gmail.com>
2025-07-14 11:55:51 +0200

    sceneChangeDetector

---
+++ include/SceneChangeDetector.hpp
@@ -0,0 +1,13 @@
+//
+//  SceneChangeDetector.hpp
+//  Film_type_classifier
+//
+//  Created by Marek Tatýrek on 14.07.2025.
+//
+
+#ifndef SceneChangeDetector_hpp
+#define SceneChangeDetector_hpp
+
+#include <stdio.h>
+
+#endif /* SceneChangeDetector_hpp */
---
+++ src/SceneChangeDetector.cpp
@@ -0,0 +1,8 @@
+//
+//  SceneChangeDetector.cpp
+//  Film_type_classifier
+//
+//  Created by Marek Tatýrek on 14.07.2025.
+//
+
+#include "SceneChangeDetector.hpp"
